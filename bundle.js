3816e8cac9f8fd64496ee9b1f1ac37f9a050c0dc
Lloyd Grubham <grubham@gmail.com>
2017-05-05 00:32:47 -0600

    Update 2017-05-05T06:32:40.182Z

--- bundle.js
+++ bundle.js
@@ -1,45 +1,81 @@
-angular.module('marsApp', ['ui.router']);
-angular.module('marsApp')
-.directive('headerDir', function () {
+'use strict';
+
+angular.module('marsApp', ['ui.router']).config(["$stateProvider", "$urlRouterProvider", function ($stateProvider, $urlRouterProvider) {
+
+    $stateProvider.state('home', {
+        url: '/',
+        templateUrl: './app/routes/home/homeTemplate.html'
+    }).state('curiosity', {
+        url: '/curiosity',
+        templateUrl: './app/routes/curiosity/curiosityTemplate.html'
+    }).state('opportunity', {
+        url: '/opportunity',
+        template: "<h2>Opportunity</h2>"
+    }).state('weather', {
+        url: '/weather',
+        template: '<h2> The current weather on mars:</h2>'
+    });
+
+    $urlRouterProvider.otherwise('/');
+}]);
+'use strict';
+
+angular.module('marsApp').directive('headerDir', function () {
     return {
         restrict: "E",
         templateUrl: 'headerTemplate.html'
-    }
-})
-angular.module('marsApp')
-.controller('marsCtrl' , ["$scope", "marsSrv", function($scope, marsSrv){
-    $scope.x = ['Kyle','Britta','Cami','Madison'];
+    };
+});
+'use strict';
 
+angular.module('marsApp').controller('marsCtrl', ["$scope", "marsSrv", function ($scope, marsSrv) {
+    $scope.x = ['Kyle', 'Britta', 'Cami', 'Madison'];
 
     $scope.getImages = function () {
         marsSrv.getImages().then(function (response) {
-            console.log(response.data.photos);
+            // console.log(response.data.photos);
             $scope.photos = response.data.photos;
-        })
+        });
     };
-
-
-    $scope.getImages();
-    
+    $scope.getWeather = function () {
+        marsSrv.getWeather().then(function (response) {
+            console.log(response);
+            $scope.weather = response;
+        }, function (response) {
+            console.log(response);
+            $scope.weather = 'Martians must be interfering with the signal. Come back later.';
+        });
+    };
+    $scope.getWeather();
+    // $scope.getImages();
 }]);
-angular.module('marsApp')
-.directive('marsDir', function () {
+'use strict';
+
+angular.module('marsApp').directive('marsDir', function () {
     return {
         restrict: 'E',
-        template: '<h6>Directive is functioning</h6>'
-    }
+        template: '<input type="text">'
+    };
 });
-angular.module('marsApp')
-.service('marsSrv' , ["$http", function($http){
-    let baseURL = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2015-6-3&api_key=';
-    let key = 'bDTHnNfXlX9xa3CilBg85EbfUe4F2gNBx8EDnZHU'
+'use strict';
 
+angular.module('marsApp').service('marsSrv', ["$http", function ($http) {
+    var baseURL = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2017-5-3&api_key=';
+    var url2 = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2017-5-3&api_key=';
+    var key = 'bDTHnNfXlX9xa3CilBg85EbfUe4F2gNBx8EDnZHU';
+    var weatherURL = 'http://marsweather.ingenology.com/v1/latest/';
 
     this.getImages = function (rover) {
         return $http({
             method: "GET",
             url: baseURL + key
-        })
-    }
+        });
+    };
 
+    this.getWeather = function () {
+        return $http({
+            method: "GET",
+            url: weatherURL
+        });
+    };
 }]);
