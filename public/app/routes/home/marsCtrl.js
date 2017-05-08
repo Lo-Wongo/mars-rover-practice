cc553c7a8d81ebedd26429bcfb4a12c8140446dc
Lloyd Grubham <grubham@gmail.com>
2017-05-08 10:27:15 -0600

    Fixed syntax error

--- public/app/routes/home/marsCtrl.js
+++ public/app/routes/home/marsCtrl.js
@@ -5,18 +5,19 @@ angular.module('marsApp')
         marsSrv.getImages().then(function (response) {
             console.log(response.data.photos);
             $scope.photos = response.data.photos;
-        }), function (response) {
+        }, function (response) {
             console.log(response)
-            $scope.imageError = 'Martians must be interfering with the signal. Please come back later.'
+            $scope.imageError = 'Martians must be interfering with the signal. Please come back later.';
         })
     };
+    
     $scope.getWeather = function(){
         marsSrv.getWeather().then(function (response) {
             console.log(response);
             $scope.weather = response.data;
         }, function (response) {
             console.log(response)
-            $scope.weatherError = 'Martians must be interfering with the signal. Please come back later.'
+            $scope.weatherError = 'Martians must be interfering with the signal. Please come back later.';
         })
     }
     $scope.getWeather();
