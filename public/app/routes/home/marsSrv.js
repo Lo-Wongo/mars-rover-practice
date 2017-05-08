d8667df1e34a7297291c1cd93b5818f025fe4732
Lloyd Grubham <grubham@gmail.com>
2017-05-08 10:40:47 -0600

    Changed weather API request to https

--- public/app/routes/home/marsSrv.js
+++ public/app/routes/home/marsSrv.js
@@ -3,7 +3,7 @@ angular.module('marsApp')
     const baseURL = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2017-5-4&api_key=';
     let url2 = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2017-5-3&api_key=';
     let key = 'bDTHnNfXlX9xa3CilBg85EbfUe4F2gNBx8EDnZHU'
-    let weatherURL = 'http://marsweather.ingenology.com/v1/latest/';
+    let weatherURL = 'https://marsweather.ingenology.com/v1/latest/';
 
 
     this.getImages = function (rover) {
