8764cb279e2dbb018ddcc42b513a0d9e405232bd
mahburg <grubham@gmail.com>
2017-05-08 10:46:12 -0600

    Changed weather api to HTTPS

--- bundle.js
+++ bundle.js
@@ -66,7 +66,7 @@ angular.module('marsApp').service('marsSrv', ["$http", function ($http) {
     var baseURL = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2017-5-4&api_key=';
     var url2 = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2017-5-3&api_key=';
     var key = 'bDTHnNfXlX9xa3CilBg85EbfUe4F2gNBx8EDnZHU';
-    var weatherURL = 'http://marsweather.ingenology.com/v1/latest/';
+    var weatherURL = 'https://marsweather.ingenology.com/v1/latest/';
 
     this.getImages = function (rover) {
         return $http({
